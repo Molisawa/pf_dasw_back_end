cb9c5fb9f44e060c70f3db7eec0d4147eb41b9df
Molisawa <ernestobvc1@gmail.com>
2022-05-07 15:39:59 -0500

    adding filters fixed update controller

--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
-import { create, findAll, findById, findByTitle, update, remove } from '../controllers/movie.controller';
+import { create, findAll, findById, findByTitle, findByActor, findByCategory, findByDirector, findByStudio, update, remove } from '../controllers/movie.controller';
 const router = Router();
 import { verifyToken, isAdmin } from '../middlewares/auth.jwt';
 
@@ -15,6 +15,18 @@ router.get('/movies/:id', asyncHandler(findById));
 
 //get movie by title
 router.post('/movies/:title', asyncHandler(findByTitle));
+
+//get movie by category
+router.get('/movies/category/:category', asyncHandler(findByCategory));
+
+//get movie by actor
+router.get('/movies/actor/:actor', asyncHandler(findByActor));
+
+//get movie by director
+router.get('/movies/director/:director', asyncHandler(findByDirector));
+
+//get movie by studio
+router.get('/movies/studio/:studio', asyncHandler(findByStudio));
 
 // update movie
 router.put('/movies/:id',[verifyToken, isAdmin], asyncHandler(update));
